8360634010b2fd81db384d3f16bc09daf366bc1a
Mikhail Burilov <burilovmv@gmail.com>
2019-08-25 16:54:48 +0700

    #1 fixed run-tests.sh - +x by default, removed sudo

--- run-tests.sh
+++ run-tests.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-sudo docker run --name oracle -d -p 1521:1521 --privileged container-registry.oracle.com/database/standard:12.1.0.2
+docker run --name oracle -d -p 1521:1521 --privileged container-registry.oracle.com/database/standard:12.1.0.2
 sleep 10
-sudo docker cp OracleSupplyCollectorTests/tests/data.sql oracle:/data.sql
-sudo docker exec -u oracle oracle sqlplus sys/Oracle@localhost @/data.sql
+docker cp OracleSupplyCollectorTests/tests/data.sql oracle:/data.sql
+docker exec -u oracle oracle sqlplus sys/Oracle@localhost @/data.sql
 dotnet test
-sudo docker stop oracle
-sudo docker rm oracle
+docker stop oracle
+docker rm oracle
